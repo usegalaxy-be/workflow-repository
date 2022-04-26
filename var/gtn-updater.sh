535a8ed5834eb3a0fcb96934281b996f5b5ff94e
bedroesb <bedro@psb.vib-ugent.be>
2022-04-26 16:35:47 +0200

    better scripts

--- var/gtn-updater.sh
+++ var/gtn-updater.sh
@@ -8,13 +8,15 @@ do
     for tutdir in $topicdir/tutorials/*
     do
         tut=$(basename $tutdir)
-        # install tools and workflows
         if [ -d $tutdir/workflows/ ];
         then
-            for w in $tutdir/workflows/*.ga
-            do
-                python3 $PWD/var/gtn-updater.py $PWD $w $topic $tut
-            done
+            if compgen -G "$tutdir/workflows/*.ga" > /dev/null; 
+            then
+                for w in $tutdir/workflows/*.ga
+                do
+                    python3 $PWD/var/gtn-updater.py $PWD $w $topic $tut
+                done
+            fi
         fi
     done
 done
